15c44f657f930273584eae864646ed4d5c915917
Ryan Donlan <donlan.ryan@gmail.com>
2021-02-24 16:51:35 -0500

    people can be drawn in groups of 1,2 or 3

--- drawing_functions.py
+++ drawing_functions.py
@@ -1,5 +1,6 @@
 import turtle
 
+
 def draw_rectangle(side1, side2, color):
     ''' draws a rectangle with side1 as horizontal and side2 as vertical'''
     turtle.setheading(0)
@@ -24,25 +25,24 @@ def draw_road(length, width, horizontal):
     turtle.forward(width/2 - 3)
     turtle.left(90)
 
-    #Orienting horizontal roads
-    turtle_distance_horz = length/10
-    while(horizontal and turtle_distance_horz < length):
-        turtle.forward(length/10)
-        draw_rectangle(length/45, width/5, 'yellow')
-        turtle_distance_horz += (length/10)
+    # #Orienting horizontal roads
+    # turtle_distance_horz = length/10
+    # while(horizontal and turtle_distance_horz < length):
+    #     turtle.forward(length/10)
+    #     draw_rectangle(length/45, width/5, 'yellow')
+    #     turtle_distance_horz += (length/10)
 
-    # Orienting vertical roads
-    turtle_distance_vert = width/10
-    turtle.forward(length/2 - 3)
-    turtle.right(90)
-    turtle.forward(450)
-    while(horizontal is False and turtle_distance_vert < width):
-        turtle.setheading(90)
-        turtle.forward(width/10)
-        draw_rectangle(length/5, width/45, 'yellow')
-        turtle_distance_vert += (width/10)
+    # # Orienting vertical roads
+    # turtle_distance_vert = width/10
+    # turtle.forward(length/2 - 3)
+    # turtle.right(90)
+    # turtle.forward(450)
+    # while(horizontal is False and turtle_distance_vert < width):
+    #     turtle.setheading(90)
+    #     turtle.forward(width/10)
+    #     draw_rectangle(length/5, width/45, 'yellow')
+    #     turtle_distance_vert += (width/10)
     
-
 
 def draw_road_system():
     '''Calls funcitons to draw road grid'''
@@ -57,6 +57,60 @@ def draw_road_system():
         draw_road(30,900, False)
 
 
+def draw_person(x_pos, y_pos):
+    '''draws a person at a specific x and y coordinate'''
+    turtle.setpos(x_pos, y_pos + 20)
+    turtle.setheading(0)
+    turtle.color('brown')
+    turtle.pendown()
+    # head
+    turtle.circle(15)
+    # torso
+    turtle.setheading(270)
+    turtle.forward(65)
+    # legs
+    turtle.setheading(235)
+    turtle.forward(45)
+    turtle.right(180)
+    turtle.penup()
+    turtle.forward(45)
+    turtle.setheading(305)
+    turtle.pendown()
+    turtle.forward(45)
+    turtle.right(180)
+    turtle.penup()
+    turtle.forward(45)
+    # arms
+    turtle.setheading(90)
+    turtle.forward(30)
+    turtle.setheading(45)
+    turtle.pendown()
+    turtle.forward(35)
+    turtle.right(180)
+    turtle.penup()
+    turtle.forward(35)
+    turtle.setheading(135)
+    turtle.pendown()
+    turtle.forward(35)
+    turtle.right(180)
+    turtle.penup()
+    turtle.forward(35)
+
+
+def draw_people(num_people,x_pos_left, y_pos_middle):
+    '''draw how may people are required
+    x_pos_left is bottom left most x_coord in grid section
+    y_pos_middle is middle y_coord in grid section'''
+    coord_iteration = 225 / (num_people + 1)
+    print(coord_iteration)
+    print(x_pos_left)
+    for i in range(1, num_people + 1):
+        draw_person(x_pos_left + (i*coord_iteration), y_pos_middle)
+
+
+
+
+
 
 
 
@@ -65,8 +119,14 @@ if __name__ == "__main__":
     screen = turtle.Screen()
     screen.setup(900, 900)
     turtle.speed(speed='fastest')
-
+    turtle.penup()
     # Drawing the roads
     draw_road_system()
+    
+
+    draw_people(2, -450, 337.5)
+
+    
+
     turtle.done()
     
