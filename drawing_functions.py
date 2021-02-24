fa98120128ebfab7b48efbb03d104160f348e3ad
Ryan Donlan <donlan.ryan@gmail.com>
2021-02-24 17:39:15 -0500

    can create a house with numerous windows

--- drawing_functions.py
+++ drawing_functions.py
@@ -1,26 +1,28 @@
 import turtle
 
 
-def draw_rectangle(side1, side2, color):
+def draw_rectangle(side1, side2, color, fill):
     ''' draws a rectangle with side1 as horizontal and side2 as vertical'''
     turtle.setheading(0)
     turtle.color(color)
     turtle.pendown()
-    turtle.begin_fill()
+    if fill is True:
+        turtle.begin_fill()
     for i in range(0,4):
         if i%2 == 0:
             turtle.forward(side1)
         else:
             turtle.forward(side2)
         turtle.right(90)
-    turtle.end_fill()
+    if fill is True:
+        turtle.end_fill()
     turtle.penup()
     turtle.setheading(0)
 
 
 def draw_road(length, width, horizontal):
     ''' Draws an individual road'''
-    draw_rectangle(length, width, 'black')
+    draw_rectangle(length, width, 'black', True)
     turtle.setheading(270)
     turtle.forward(width/2 - 3)
     turtle.left(90)
@@ -29,7 +31,7 @@ def draw_road(length, width, horizontal):
     # turtle_distance_horz = length/10
     # while(horizontal and turtle_distance_horz < length):
     #     turtle.forward(length/10)
-    #     draw_rectangle(length/45, width/5, 'yellow')
+    #     draw_rectangle(length/45, width/5, 'yellow', True)
     #     turtle_distance_horz += (length/10)
 
     # # Orienting vertical roads
@@ -40,7 +42,7 @@ def draw_road(length, width, horizontal):
     # while(horizontal is False and turtle_distance_vert < width):
     #     turtle.setheading(90)
     #     turtle.forward(width/10)
-    #     draw_rectangle(length/5, width/45, 'yellow')
+    #     draw_rectangle(length/5, width/45, 'yellow', True)
     #     turtle_distance_vert += (width/10)
     
 
@@ -102,11 +104,76 @@ def draw_people(num_people,x_pos_left, y_pos_middle):
     x_pos_left is bottom left most x_coord in grid section
     y_pos_middle is middle y_coord in grid section'''
     coord_iteration = 225 / (num_people + 1)
-    print(coord_iteration)
-    print(x_pos_left)
     for i in range(1, num_people + 1):
         draw_person(x_pos_left + (i*coord_iteration), y_pos_middle)
 
+
+def draw_windows(num_windows, x_pos_left, y_pos_middle):
+    '''draw the amount of windows required on the house
+    x_pos_left is bottom left most x_coord in grid section
+    y_pos_middle is middle y_coord in grid section'''
+    coord_iteration = 225 / (num_windows + 1)
+    turtle.setpos(x_pos_left + 20/num_windows + coord_iteration/2 ,y_pos_middle - 15)
+    turtle.setheading(0)
+    turtle.color('black')
+    for i in range(1, num_windows + 1):
+        draw_rectangle(26, 26, 'black', False)
+        turtle.forward(13)
+        turtle.right(90)
+        turtle.pendown()
+        turtle.forward(26)
+        turtle.penup()
+        turtle.left(90)
+        turtle.forward(13)
+        turtle.left(90)
+        turtle.forward(13)
+        turtle.left(90)
+        turtle.pendown()
+        turtle.forward(26)
+        turtle.penup()
+        turtle.right(90)
+        turtle.forward(13)
+        turtle.right(90)
+        turtle.forward(coord_iteration)
+
+
+
+def draw_house(num_windows, x_pos_left, y_pos_middle):
+    '''draw a house with as many windows required
+    x_pos_left is bottom left most x_coord in grid section
+    y_pos_middle is middle y_coord in grid section'''
+    turtle.setpos(x_pos_left + 20, y_pos_middle)
+    # house
+    draw_rectangle(175, 100, 'pink', True)
+    # roof
+    turtle.color('grey')
+    turtle.pendown()
+    turtle.begin_fill()
+    turtle.setheading(35)
+    turtle.forward(106.82)
+    turtle.right(70)
+    turtle.forward(106.82)
+    turtle.right(145)
+    turtle.forward(175)
+    turtle.end_fill()
+    turtle.penup()
+    # door
+    turtle.setpos(x_pos_left + 95.5, y_pos_middle - 55)
+    turtle.setheading(90)
+    draw_rectangle(30, 45, 'brown', True)
+    turtle.setpos(x_pos_left+107.5, y_pos_middle - 80)
+    turtle.color('yellow')
+    turtle.pendown()
+    turtle.begin_fill()
+    turtle.circle(3)
+    turtle.end_fill()
+    turtle.penup()
+    # windows
+    draw_windows(num_windows, x_pos_left, y_pos_middle)
+
+
+
+    
 
 
 
@@ -123,8 +190,7 @@ if __name__ == "__main__":
     # Drawing the roads
     draw_road_system()
     
-
-    draw_people(2, -450, 337.5)
+    draw_house(3, -450, 345)
 
     
 
